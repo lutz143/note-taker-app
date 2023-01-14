4ce59ec3f8990e57d8a284cbf7bc37d3426c942d
Josh <lutz_143@yahoo.com>
2023-01-14 13:43:24 -0500

    minor updates to get on server.js to return notes page

--- server.js
+++ server.js
@@ -1,29 +1,30 @@
 // require express and path modules
 const express = require('express');
 const path = require('path');
+const api = require('./routes/db.js');
 
 // create port for local listening while in development
 const PORT = 3001;
 
 const app = express();
 
-// create express middleware to prepare body and query
+// create express middleware to prepare body, query, and custom route middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use('/api', api);
 
 // create routes in the public folder
 app.use(express.static('public'));
 
-// get index.html
+// get index.html or the route for the homepage
 app.get('/', (req, res) =>
   res.sendFile(path.join(__dirname, '/public/index.html'))
 );
 
-// GET request for notes
-app.get('/api/notes', (req, res) => {
-  console.info(`GET /api/notes`);
-  res.status(200).json(notes);
-});
+// GET request for notes page
+app.get('/notes', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/notes.html'))
+);
 
 // listen to const PORT
 app.listen(PORT, () =>
