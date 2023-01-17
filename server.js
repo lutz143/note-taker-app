b94b68ff259e0815dd257ddd66271ab8970fd217
Josh <lutz_143@yahoo.com>
2023-01-17 12:33:57 -0500

    updates to port

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const api = require('./routes/db.js');
 let notes = require('./db/db.json');
 
 // create port for local listening while in development
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT;
 
 const app = express();
 
