0cba11ce089f06e58523c86a62c0cc76a4d074eb
Josh <lutz_143@yahoo.com>
2023-01-17 19:39:16 -0500

    minor updates to server.js

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const api = require('./routes/db.js');
 let notes = require('./db/db.json');
 
 // create port for local listening while in development
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT||3001;
 
 const app = express();
 
@@ -18,9 +18,9 @@ app.use('/api/notes', api);
 app.use(express.static('public'));
 
 // get index.html or the route for the homepage
-app.get('/', (req, res) =>
-  res.sendFile(path.join(__dirname, '/public/index.html'))
-);
+// app.get('/', (req, res) =>
+//   res.sendFile(path.join(__dirname, '/public/index.html'))
+// );
 
 // GET request for notes page
 app.get('/notes', (req, res) =>
@@ -41,9 +41,8 @@ app.get('/api/notes/:id', (req, res) => {
 });
 
 // wildcard route to direct users to main page
-// Wildcard route to direct users to a 404 page
 app.get('*', (req, res) =>
-  res.sendFile(path.join(__dirname, 'public/pages/404.html'))
+  res.sendFile(path.join(__dirname, '/public/index.html'))
 );
 
 // listen to const PORT
